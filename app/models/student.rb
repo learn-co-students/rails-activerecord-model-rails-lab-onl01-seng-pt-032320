c26ab3dc4dc4ba2506f2122c08c7ee0be5a7c96a
github username <github email address>
2020-08-10 10:56:42 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,9 +1,13 @@
 class Student < ActiveRecord::Base
 
-    def @to_s
-        str = ""
-        str.concat("first_name, last_name")
-        str
+    # def to_s
+    #     str = ""
+    #     str.concat("first_name, last_name")
+    #     str
+    # end
+
+    def to_s
+        @student = Student.create!(first_name: " ", last_name: " ")
     end
   end
   
